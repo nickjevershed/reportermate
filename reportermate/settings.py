554f7cb70956ea50e30669b38da032e1f50f1734
Nick Evershed <nick.evershed@theguardian.com>
2017-08-05 11:52:00 +1000

    vars

--- reportermate/settings.py
+++ reportermate/settings.py
@@ -21,7 +21,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+
+SECRET_KEY = os.environ['REPORTERMATE_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
